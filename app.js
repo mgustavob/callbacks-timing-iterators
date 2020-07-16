cc718cf779871347ccf9fce064fa9a54c03382ff
Martin Briceno <martin.briceno@live.com>
2020-07-16 18:10:52 -0400

    First call Back

---
+++ app.js
@@ -0,0 +1,15 @@
+function greeting(name, callback) {
+    let sentence = 'Hello, my name is ' + name+ ' ';
+    let otherSentence = callback();
+    return sentence + otherSentence;
+}
+
+
+function anotherGreeting() {
+    let sentence = 'I forgot my last name';
+
+    return sentence;
+}
+
+let myName = 'Martin';
+console.log(greeting(myName, anotherGreeting));
